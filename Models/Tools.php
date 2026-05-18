032589a8db38c1c3b66cf81c37223de2345fe601
Léa Martinaud <lea.caille44@gmail.com>
2026-05-18 16:38:12 +0200

    Léa Cas 2 3 Detail et Creation

--- Models/Tools.php
+++ Models/Tools.php
@@ -33,24 +33,134 @@ class Tools
         };
         var_dump($sql);
 
-        $resultsql = $this->db->requete($sql);
+        $result_sql = $this->db->requete($sql);
 
-        if (!isset($resultsql)) {
-            $resultrsql = "Aucun resultat est ressorti";
+        if (!isset($result_sql)) {
+            $resultsql = "Aucun resultat est ressorti";
         }
 
         $sql = "
             SELECT * 
             FROM tools
             ";
-        $resulttotal = $this->db->requete($sql);
+        $result_total = $this->db->requete($sql);
 
         $result = [
-            "data" => $resultsql,
-            "filtered" => count($resultsql),
-            "total" => count($resulttotal),
+            "data" => $result_sql,
+            "filtered" => count($result_sql),
+            "total" => count($result_total),
         ];
 
         var_dump($result);
     }
+
+    public function researchById(int $id)
+    {
+        $sql = "
+            SELECT * 
+            FROM tools
+            WHERE id = '$id'
+            ";
+        $result = $this->db->requete($sql);
+        if (empty($result)) {
+            $result = "Il n'y a pas de résultat";
+        }
+
+        // $sql = "
+        //     SELECT SUM(active_users_count) AS total_sessions
+        //     FROM cost_tracking
+        //     WHERE tool_id = $id
+        //     ";
+        // $result_total = $this->db->requete($sql);
+
+        var_dump($result);
+        // var_dump($result_total);
+    }
+
+    public function creationOutil(string $name, string $description = null, string $vendor, string $website_url = null, int $monthly_cost, string $owner_department)
+    {
+        // var_dump($name, $description, $vendor, $website_url, $monthly_cost, $owner_department);
+
+        $category_id = 0;
+        switch ($owner_department) {
+            case "communication":
+                $category_id = 1;
+                break;
+
+            case "development":
+                $category_id = 2;
+                break;
+
+            case "design":
+                $category_id = 3;
+                break;
+
+            case "productivity":
+                $category_id = 4;
+                break;
+
+            case "analytics":
+                $category_id = 5;
+                break;
+
+            case "security":
+                $category_id = 6;
+                break;
+
+            case "marketing":
+                $category_id = 7;
+                break;
+
+            case "hr":
+                $category_id = 8;
+                break;
+
+            case "finance":
+                $category_id = 9;
+                break;
+
+            case "infrastructure":
+                $category_id = 10;
+                break;
+
+            default:
+                echo "Il y a un probléme au niveau de la catégorie";
+                break;
+        };
+
+        if (
+            empty(trim($name)) ||
+            empty(trim($vendor)) ||
+            empty($monthly_cost) ||
+            empty(trim($owner_department)) ||
+            empty($category_id) 
+        ) {
+            die("Tous les champs sont obligatoires");
+        }
+
+        if (strlen($name) < 2 || strlen($name) > 100) {
+            die("Nom invalide (2 à 100 caractères)");
+        }
+        if (strlen($vendor) > 100) {
+            die("Nom invalide (2 à 100 caractères)");
+        }
+
+        if (!is_numeric($monthly_cost) || $monthly_cost < 0) {
+            die("Le coût doit être un nombre positif");
+        }
+
+        if (!is_numeric($category_id) || $category_id < 0) {
+            die("Le coût doit être un nombre");
+        }
+
+        $sql = "
+        INSERT INTO tools
+        (name, description, vendor, website_url, category_id,monthly_cost,active_users_count,owner_department,status)
+        VALUES
+        ('$name','$description','$vendor','$website_url','$category_id','$monthly_cost', '0', '$owner_department',NULL)
+        ";
+        $result = $this->db->requete($sql);
+    }
+
+    
 }
--- index.php
+++ index.php
@@ -15,15 +15,70 @@ use App\Models\Tools;
 $tools = new Tools();
 
 ?>
+<h1>Phase 3 API</h1>
 
-<p></p> Cas Sandra - Listes avec filtres
+<h2>Cas Sandra - Listes avec filtres</h2>
 
 <?php  
 $order = "monthly_cost";
 
-$tools->liste_avec_filtre($order,["owner_department = 'Engineering'","status = 'active'"]);
+// $tools->liste_avec_filtre($order,["owner_department = 'Engineering'","status = 'active'"]);
+$tools->liste_avec_filtre($order,["id = 1"]);
 ?>
 
+<h2>Cas Marcus - Details complet</h2>
+
+<!-- Phase 2 incomplete pas trouver comment crée "total_sessions" et "avg_session_minutes" -->
+<?php 
+$id = 5;
+
+$tools->researchById($id);
+
+?>
+
+<h2>Cas Lisa - Création nouvel outil</h2>
+
+<form method="POST">
+    <label for="name">Nom</label>
+    <input type="text" id="name" placeholder="Nom de l'application" name="name" minlength="2" maxlength="100" required><br>
+    <label for="description">Description</label>
+    <input type="text" id="description" placeholder="Description de l'application" name="description"><br>
+    <label for="vendor">Fournisseur</label>
+    <input type="text" id="vendor" placeholder="Fournisseur de l'application" name="vendor" maxlength="100"><br>
+    <label for="website_url">Lien URL</label>
+    <input type="url" id="website_url" placeholder="Lien URL vers l'application" name="website_url"><br>
+    <label for="monthly_cost">Coût-mensuelle</label>
+    <input type="number" id="monthly_cost" placeholder="Coût-mensuelle de l'application" name="monthly_cost"><br>
+    <label for="owner_department">Service</label>
+    <select name="owner_department" id="owner_department">
+        <option value="communication">Communication</option>
+        <option value="development">Developpement</option>
+        <option value="design">Designer</option>
+        <option value="productivity">Productivité</option>
+        <option value="analytics">Analitique</option>
+        <option value="security">Sécurité</option>
+        <option value="marketing">Marketing</option>
+        <option value="hr">Ressources humaines</option>
+        <option value="finance">Finance</option>
+        <option value="infrastructure">Infrastructure</option>
+    </select><br>
+    <button type="submit" name="inscription">Envoyer</button>
+    <?php 
+
+ if (isset($_POST["inscription"])) {
+
+    $name=trim($_POST["name"]);
+    $description= trim($_POST["description"]);
+    $vendor= trim($_POST["vendor"]);
+    $website_url=trim($_POST["website_url"]);
+    $monthly_cost= (float)$_POST["monthly_cost"];
+    $owner_department= trim($_POST["owner_department"]);
+
+    $tools->creationOutil($name,$description,$vendor,$website_url,$monthly_cost,$owner_department);
+}
+
+?>
+</form>
 
 </body>
 </html>
